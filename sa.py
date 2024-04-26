70da20739b9bfc2a427a65424f2af22a9bd2d483
xuzel <sudooodus@gmail.com>
2024-04-27 03:25:43 +0800

    add sa algorithm and fix fit function make it suitable for sa

--- sa.py
+++ sa.py
@@ -1,39 +1,28 @@
-from sko.PSO import PSO
+import numpy as np
+from sko.SA import SA
 import matplotlib.pyplot as plt
+import pandas as pd
 from data_structure import SatInfo, fitness_func_with_param, TEST_SET, hash_function
 from time import perf_counter
-
-
-
 
 
 def main():
     start_time = perf_counter()
     sat_info = TEST_SET[hash_function(4)]
     n_dim = sat_info.get_input_len()
-
-    pso = PSO(func=fitness_func_with_param(sat_info),
-              n_dim=n_dim,
-              pop=200,
-              max_iter=200,
-              lb=[0] * n_dim,
-              ub=[1] * n_dim,
-              w=0.9,
-              c1=2,
-              c2=2)
-
-    solution = pso.run()
-    end_time = perf_counter()
-    print(solution[0])
-    solution = [round(x) for x in solution[0]]
-    # print(solution)
+    sa = SA(func=fitness_func_with_param(sat_info),
+            x0=np.random.choice([0, 1], size=n_dim),
+            T_max=1,
+            T_min=1e-9,
+            L=300,
+            max_stay_counter=150)
+    solution = sa.run()[0]
+    solution = [round(x) for x in solution]
     print(f'the solution is:\n{sat_info.choose_list(solution)}\n{solution}\n')
     print(f'the number of the solution is {sum(solution)}')
     print(f'valid the solution is {sat_info.all_j_subsets_covered(solution)}')
-
-    print(f"run time: {end_time - start_time} seconds")
-
-    plt.plot(pso.gbest_y_hist)
+    print(f'the time run is {perf_counter() - start_time} second')
+    plt.plot(pd.DataFrame(sa.best_y_history).cummin(axis=0))
     plt.show()
 
 
