e556ba198eba3841835893f779fa3b5e76f3ddce
zqq <qianqizhang.must@gmail.com>
2024-04-27 16:23:11 +0800

    Refactor code: Add tqdm library for progress bar in sa.py and pso.py

--- algorithms/ga.py
+++ algorithms/ga.py
@@ -11,62 +11,45 @@ import os
 from .data_structure import SatInfo, fitness_func_with_param
 from .utils import TEST_SET, hash_function, save_result_to_file
 
-def fitness_func_with_param(set_info: SatInfo):
-    def fitness_func(solution):
-        if set_info.all_j_subsets_covered(solution):
-            return sum(solution)
-        else:
-            return math.inf
-
-    return fitness_func
-
-
-# def greedy_set_cover(cover_graph):
-#     covered = set()
-#     selected_k_combs = []
-#     while any(j_sub not in covered for j_subsets in cover_graph.values() for j_sub in j_subsets):
-#         best_k_comb = max(cover_graph, key=lambda k: len(set(cover_graph[k]) - covered))
-#         selected_k_combs.append(best_k_comb)
-#         covered.update(cover_graph[best_k_comb])
-#     return selected_k_combs
-
-
 def main(iteration, param_version, output_folder):
     algorithm = 'GA'
 
     start_time = perf_counter()
-    sat_info = SatInfo(45, 9, 6, 4, 4)
 
-    #参数：种群大小，迭代次数，变异概率
-    gene_size = sat_info.get_input_len()
-    num_generations = 600
-    prob_mut=0.01,
-    weight = 20 
-    size_pop = int(math.sqrt(gene_size) * weight)
-    size_pop = size_pop if size_pop % 2 == 0 else size_pop + 1
+    for i in tqdm(range(1, 7)):
+        sat_info = TEST_SET[hash_function(i)]
+
+        #参数：种群大小，迭代次数，变异概率
+        gene_size = sat_info.get_input_len()
+        num_generations = 600
+        prob_mut=0.01,
+        weight = 20 
+        size_pop = int(math.sqrt(gene_size) * weight)
+        size_pop = size_pop if size_pop % 2 == 0 else size_pop + 1
 
 
-    ga = GA(func=fitness_func_with_param(sat_info),
-            n_dim=gene_size,
-            size_pop=size_pop,
-            max_iter=num_generations,
-            prob_mut=prob_mut,  # 0.001
-            lb=[0] * sat_info.get_input_len(),
-            ub=[1] * sat_info.get_input_len(),
-            precision=1)
-    solution = ga.run()
-    run_time = perf_counter() - start_time
+        ga = GA(func=fitness_func_with_param(sat_info),
+                n_dim=gene_size,
+                size_pop=size_pop,
+                max_iter=num_generations,
+                prob_mut=prob_mut,  # 0.001
+                lb=[0] * sat_info.get_input_len(),
+                ub=[1] * sat_info.get_input_len(),
+                precision=1)
+        solution = ga.run()
+        run_time = perf_counter() - start_time
 
-    # Store the solution and time in a dictionary
-    result = {
-        'gene_size': gene_size, 
-        'num_generations': num_generations, 
-        'prob_mut': prob_mut, 
-        'size_pop': size_pop,
-        'solution': sum(solution[0]), 
-        'time': run_time
-        }
-    save_result_to_file(algorithm, iteration, param_version, result, output_folder, ga)
+        # Store the solution and time in a dictionary
+        result = {
+            'qusetion_num': i,
+            'gene_size': gene_size, 
+            'num_generations': num_generations, 
+            'prob_mut': prob_mut, 
+            'size_pop': size_pop,
+            'solution': sum(solution[0]), 
+            'time': run_time
+            }
+        save_result_to_file(algorithm, iteration, param_version, result, output_folder, ga, qusetion_num=i)
 
 # if __name__ == '__main__':
 #     i = 5  # Number of iterations
--- algorithms/pso.py
+++ algorithms/pso.py
@@ -1,6 +1,7 @@
 from sko.PSO import PSO
 import matplotlib.pyplot as plt
 from time import perf_counter
+from tqdm import tqdm
 
 from .data_structure import SatInfo, fitness_func_with_param
 from .utils import TEST_SET, hash_function,save_result_to_file
@@ -10,52 +11,54 @@ def main(iteration, param_version, output_folder):
     algorithm = 'PSO'
 
     start_time = perf_counter()
-    sat_info = TEST_SET[hash_function(4)]
-    n_dim = sat_info.get_input_len()
 
-    pop = 200
-    max_iter = 200
-    w = 0.9
-    c1 = 2
-    c2 = 2
+    for i in tqdm(range(1, 7)):
+        sat_info = TEST_SET[hash_function(i)]
 
-    pso = PSO(func=fitness_func_with_param(sat_info),
-              n_dim=n_dim,
-              pop=pop,
-              max_iter=max_iter,
-              lb=[0] * n_dim,
-              ub=[1] * n_dim,
-              w=w,
-              c1=c1,
-              c2=c2)
+        n_dim = sat_info.get_input_len()
+        pop = 200
+        max_iter = 200
+        w = 0.9
+        c1 = 2
+        c2 = 2
 
-    solution = pso.run()
-    end_time = perf_counter()
-    run_time = perf_counter() - start_time
-    solution = [round(x) for x in solution[0]]
+        pso = PSO(func=fitness_func_with_param(sat_info),
+                n_dim=n_dim,
+                pop=pop,
+                max_iter=max_iter,
+                lb=[0] * n_dim,
+                ub=[1] * n_dim,
+                w=w,
+                c1=c1,
+                c2=c2)
 
-    result = {
-        'n_dim': n_dim,
-        'pop': pop,
-        'max_iter': max_iter,
-        'w': w,
-        'c1': c1,
-        'c2': c2,
-        'solution': sum(solution),
-        'time': run_time
-    }
-    save_result_to_file(algorithm, iteration, param_version, result, output_folder, pso)
+        solution = pso.run()
+        end_time = perf_counter()
+        run_time = perf_counter() - start_time
+        solution = [round(x) for x in solution[0]]
 
-    if Debug:
-        # print(solution)
-        print(f'the solution is:\n{sat_info.choose_list(solution)}\n{solution}\n')
-        print(f'the number of the solution is {sum(solution)}')
-        print(f'valid the solution is {sat_info.all_j_subsets_covered(solution)}')
+        result = {
+            'n_dim': n_dim,
+            'pop': pop,
+            'max_iter': max_iter,
+            'w': w,
+            'c1': c1,
+            'c2': c2,
+            'solution': sum(solution),
+            'time': run_time
+        }
+        save_result_to_file(algorithm, iteration, param_version, result, output_folder, pso, qusetion_num=i)
 
-        print(f"run time: {end_time - start_time} seconds")
+        if Debug:
+            # print(solution)
+            print(f'the solution is:\n{sat_info.choose_list(solution)}\n{solution}\n')
+            print(f'the number of the solution is {sum(solution)}')
+            print(f'valid the solution is {sat_info.all_j_subsets_covered(solution)}')
 
-        plt.plot(pso.gbest_y_hist)
-        plt.show()
+            print(f"run time: {end_time - start_time} seconds")
+
+            plt.plot(pso.gbest_y_hist)
+            plt.show()
 
 
 # if __name__ == '__main__':
