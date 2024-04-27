b7f9729fad4f265db2b207339bc962b2cf61b75d
xuzel <sudooodus@gmail.com>
2024-04-28 01:05:21 +0800

    fix print result default parm

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from algorithms import *
+#
+# run_ga(
+#     [45, 8, 6, 4, 4],
+# ).print_result(True)
+#
+# run_aca(
+#     [45, 8, 6, 4, 4]
+# ).print_result(True)
+#
+# run_asfa(
+#     [45, 8, 6, 4, 4]
+# ).print_result(True)
+#
+# run_pso(
+#     [45, 8, 6, 4, 4]
+# ).print_result(True)
+#
+# run_sa(
+#     [45, 8, 6, 4, 4]
+# ).print_result(True)
 
-run_ga(
-    [45, 8, 6, 4, 4],
-    auto_parm=False,
-    max_iter=100
-).print_result(True)
-
-run_aca(
-    [45, 8, 6, 4, 4]
-).print_result(True)
-
-run_asfa(
-    [45, 8, 6, 4, 4]
-).print_result(True)
-
-run_pso(
-    [45, 8, 6, 4, 4]
-).print_result(True)
-
-run_sa(
-    [45, 8, 6, 4, 4]
-).print_result(True)
+result = run_ga([45, 8, 6, 4, 4])
+result.print_result()
+print(result.solution)
