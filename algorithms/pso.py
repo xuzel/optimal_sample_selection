580188d07dc6a160cc38f8ad07abfc8f52218cf7
xuzel <sudooodus@gmail.com>
2024-04-28 16:35:02 +0800

    fix np.ndarry can't round() error

--- algorithms/pso.py
+++ algorithms/pso.py
@@ -36,6 +36,8 @@ def run_pso(sample_parm: typing.List[int],
         pso.X = replace_rows_randomly(pso.X, greedy_replace_probability, greedy_solution)
         # print(pso.X)
     solution = pso.run()[0]
+    if type(solution) is np.ndarray:
+        solution = solution.tolist()
     solution = [round(x) for x in solution]
     end_time = perf_counter()
     result = Result(
