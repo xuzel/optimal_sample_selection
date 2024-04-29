df2a420425636f6907e0f839f07d6f6ffd33b698
xuzel <sudooodus@gmail.com>
2024-04-29 21:47:31 +0800

    add parm custom_arr

--- algorithms/pso.py
+++ algorithms/pso.py
@@ -14,9 +14,10 @@ def run_pso(sample_parm: typing.List[int],
             max_iter=100,
             w=0.9,
             c1=5,
-            c2=5):
+            c2=5,
+            **kwargs):
     start_time = perf_counter()
-    sat_info = SatInfo(*sample_parm)
+    sat_info = SatInfo(*sample_parm, **kwargs)
     n_dim = sat_info.get_input_len()
     pso = PSO(
         func=fitness_func_with_param(sat_info),
